49c60203f234434f976c5017efb42d08de97fa8a
chadlia <chadlia@user-TOSH>
2014-04-04 00:00:08 +0200

    new commit by chadlia

---
+++ src/voyagepi-dev/Midlet.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package voyagepi;
+
+
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author chadlia
+ */
+public class Midlet extends MIDlet {
+
+    public void startApp() {
+        
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}
